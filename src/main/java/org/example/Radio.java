33966045214eda32c7296eae89cb87cf05f398a1
Mihail Test <misha.lucenko17@gmail.com>
2024-06-20 19:50:06 +0300

    New commit

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,4 +1,52 @@
 package org.example;
 
 public class Radio {
+
+    public int numberCurrentRadioStation;
+    public int soundVolume;
+    public int decreaseVolume;
+
+    public void setNumberCurrentRadioStation(int newNumberCurrentRadioStation) {
+        if (newNumberCurrentRadioStation >= 9) {
+            newNumberCurrentRadioStation = 0;
+            return;
+        }
+        if (newNumberCurrentRadioStation <= 0) {
+            newNumberCurrentRadioStation = 9;
+        }
+        numberCurrentRadioStation = newNumberCurrentRadioStation;
+    }
+
+    public int buttonNext() {
+        int next = numberCurrentRadioStation;
+        if (numberCurrentRadioStation == 0) {
+        } else {
+            next = numberCurrentRadioStation + 1;
+        }
+        return next;
+    }
+
+    public int buttonPrev() {
+        int prev = 0;
+        if (numberCurrentRadioStation == 9) {
+            return numberCurrentRadioStation;
+        } else {
+            prev = numberCurrentRadioStation - 1;
+        }
+        return prev;
+    }
+
+    public int increaseVolume() {
+        if (soundVolume < 100) {
+            soundVolume = soundVolume + 1;
+        }
+        return soundVolume;
+    }
+
+    public int reduceVolume() {
+        if (decreaseVolume > 0) {
+            decreaseVolume = decreaseVolume - 1;
+        }
+        return decreaseVolume;
+    }
 }
--- src/test/java/org/example/RadioTest.java
+++ src/test/java/org/example/RadioTest.java
@@ -1,4 +1,99 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioTest {
+
+    @Test
+    public void buttonNext() {
+        Radio next = new Radio();
+
+        next.setNumberCurrentRadioStation(7);
+
+        int expected = 8;
+        int actual = next.buttonNext();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void borderButtonNext() {
+        Radio next = new Radio();
+
+        next.setNumberCurrentRadioStation(9);
+
+        int expected = 0;
+        int actual = next.buttonNext();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buttonPrev() {
+        Radio prev = new Radio();
+
+        prev.setNumberCurrentRadioStation(8);
+
+        int expected = 7;
+        int actual = prev.buttonPrev();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void borderButtonPrev() {
+        Radio prev = new Radio();
+
+        prev.setNumberCurrentRadioStation(0);
+
+        int expected = 9;
+        int actual = prev.buttonPrev();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void soundVolume() {
+        Radio sound = new Radio();
+
+        sound.soundVolume = 99;
+        int expected = 100;
+        int actual = sound.increaseVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void borderSoundVolume() {
+        Radio sound = new Radio();
+
+        sound.soundVolume = 100;
+        int expected = 100;
+        int actual = sound.increaseVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decreaseVolume() {
+        Radio decrease = new Radio();
+
+        decrease.decreaseVolume = 1;
+        int expected = 0;
+        int actual = decrease.reduceVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void borderDecreaseVolume() {
+        Radio decrease = new Radio();
+
+        decrease.decreaseVolume = 0;
+        int expected = 0;
+        int actual = decrease.reduceVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
